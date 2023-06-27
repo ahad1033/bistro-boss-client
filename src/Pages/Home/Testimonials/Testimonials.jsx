3fdc3abd5acaaed7d784e84c536f8226cbd91d99
ahad1033 <a.ahad2232@gmail.com>
2023-06-28 03:26:45 +0600

    testimoniaal modified and font awesome installed-74/9

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -6,15 +6,20 @@ import "swiper/css/navigation";
 
 import { Navigation } from "swiper";
 import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
 
 const Testimonials = () => {
 
     const [reviews, setReviews] = useState([]);
 
-    useEffect( () => {
+    useEffect(() => {
         fetch('reviews.json')
-        .then(res => res.json())
-        .then(data => setReviews(data))
+            .then(res => res.json())
+            .then(data => setReviews(data))
     }, [])
 
     return (
@@ -28,10 +33,15 @@ const Testimonials = () => {
                     reviews.map(review => <SwiperSlide
                         key={review._id}
                     >
-                        <div className="m-24">
-
-                            <p>{review.details}</p>
-                            <h3 className="text-2xl text-orange-400">{review.name}</h3>
+                        <div className="flex flex-col items-center mx-24 my-16">
+                            <Rating
+                                style={{ maxWidth: 180 }}
+                                value={review.rating}
+                                readOnly
+                            />
+                            <FontAwesomeIcon className="text-9xl mt-4" icon={faQuoteLeft} style={{color: "#000000",}} />
+                            <p className="py-8 text-center">{review.details}</p>
+                            <h3 className="text-3xl font-bold text-orange-400">{review.name}</h3>
                         </div>
                     </SwiperSlide>)
                 }
