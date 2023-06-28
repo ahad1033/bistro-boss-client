77c2e92010b7eadc79bf35597813a2844548a76d
ahad1033 <a.ahad2232@gmail.com>
2023-06-29 03:51:34 +0600

    75/4-4.5

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,15 +1,27 @@
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from '../../../assets/menu/banner3.jpg'
-import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+import useMenu from "../../../Hooks/useMenu";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 const Menu = () => {
+
+    const [menu] = useMenu();
+    const desert = menu.filter(item => item.category === 'desert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
+
     return (
         <div>
             <Cover
                 img={menuImg}
                 title="our menu"
             />
-            <PopularMenu />
+            <SectionTitle
+                subHeading="Don't Miss"
+                heading="Today's Offer"
+            />
         </div>
     );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,8 +1,18 @@
 
-const MenuCategory = () => {
+
+const MenuCategory = ({ items }) => {
+
+
     return (
         <div>
-            
+            <div className="grid md:grid-cols-2 gap-10">
+                {
+                    popular.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    />)
+                }
+            </div>
         </div>
     );
 };
