51115a74846c169d5d0d0140c35fead558b439d9
ahad1033 <a.ahad2232@gmail.com>
2023-06-29 03:02:53 +0600

    cover modified-75/2

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,8 +1,15 @@
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
     return (
         <div>
-            <h3>Menu</h3>
+            <Cover
+                img={menuImg}
+                title="our menu"
+            />
+            <PopularMenu />
         </div>
     );
 };
