c9be548cd41d9c7194ea9944a30bb0ff145dfbf5
ahad1033 <a.ahad2232@gmail.com>
2023-06-29 17:15:55 +0600

    foodCard added-75/5

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,18 +1,30 @@
+import { useState } from 'react';
 import orderCover from '../../../assets/shop/order.jpg'
 import Cover from '../../Shared/Cover/Cover';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const Order = () => {
+
+    const [tabIndex, setTabIndex] = useState(0);
+
     return (
         <div>
             <Cover
                 img={orderCover}
                 title={"Order Food"}
             />
-            <div className="tabs">
-                <a className="tab tab-lifted">Tab 1</a>
-                <a className="tab tab-lifted tab-active">Tab 2</a>
-                <a className="tab tab-lifted">Tab 3</a>
-            </div>
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>Salad</Tab>
+                    <Tab>Pizza</Tab>
+                    <Tab>Dessert</Tab>
+                    <Tab>Soup</Tab>
+                    <Tab>Drinks</Tab>
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
         </div>
     );
 };
