c400339e6b9b99f20d2b6f305819eeb74c9287e6
ahad1033 <a.ahad2232@gmail.com>
2023-07-29 00:21:01 +0600

    create dashboard route and dashboard navigation-77/6

--- src/Routes/Routers.jsx
+++ src/Routes/Routers.jsx
@@ -7,6 +7,8 @@ import Menu from "../Pages/Menu/Menu/Menu";
 import Order from "../Pages/Order/Order/Order";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
+import Dashboard from "../Layout/Dashboard";
+import MyCart from "../Pages/Dashboard/MyCart/MyCart";
 
   export const router = createBrowserRouter([
     {
@@ -35,4 +37,14 @@ import SignUp from "../Pages/SignUp/SignUp";
         }
       ]
     },
+    {
+      path: 'dashboard',
+      element: <Dashboard />,
+      children: [
+        {
+          path: 'mycart',
+          element: <MyCart />
+        }
+      ]
+    }
   ]);
