fe295ac097f8cf7806f839f333dcd89766b7b1f8
ahad1033 <a.ahad2232@gmail.com>
2023-07-28 23:02:09 +0600

    tanstack query used to load data-77/4

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,21 @@ import {
 import './index.css'
 import { router } from './Routes/Routers';
 import AuthProvider from './Providers/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <div className='max-w-screen-xl mx-auto'>
-        <RouterProvider router={router} />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className='max-w-screen-xl mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>,
 )
