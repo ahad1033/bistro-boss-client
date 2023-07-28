eda589a511a93c9fe9155c010bfe3a5ddd0b3317
ahad1033 <a.ahad2232@gmail.com>
2023-07-28 17:16:56 +0600

    handle add to cart setup-77/2

---
+++ src/Components/FoodCard/FoodCard.jsx
@@ -0,0 +1,54 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
+import Swal from "sweetalert2";
+
+const FoodCard = ({ item }) => {
+
+    const { name, image, price, recipe } = item;
+    const { user } = useContext(AuthContext);
+
+    const handleAddToCart = item => {
+        console.log(item);
+        if(user) {
+            fetch('http://localhost:5000/carts')
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedID) {
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Successfully added to cart',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
+            })
+        }
+        else{
+            Swal.fire({
+                title: 'Please Login to Order Food',
+                showClass: {
+                  popup: 'animate__animated animate__fadeInDown'
+                },
+                hideClass: {
+                  popup: 'animate__animated animate__fadeOutUp'
+                }
+              })
+        }
+    }
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src={image} alt="Food" /></figure>
+            <p className="bg-slate-900 text-white absolute right-0 mr-4 mt-4 px-4 rounded">${price}</p>
+            <div className="card-body flex flex-col items-center">
+                <h2 className="card-title">{name}</h2>
+                <p>{recipe}</p>
+                <div className="card-actions justify-end">
+                    <button onClick={() => handleAddToCart(item) } className="btn btn-outline border-0 bg-slate-100 border-orange-400 border-b-4 text-orange-500 mt-3">Add to cart</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FoodCard;
