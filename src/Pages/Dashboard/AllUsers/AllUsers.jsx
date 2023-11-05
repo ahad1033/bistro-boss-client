b1ff5da76469ce6a82326b507a0aa379806df7bb
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-11-05 20:15:50 +0600

    make user admin-78-6

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { FaTrashAlt, FaUserShield } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const AllUsers = () => {
 
@@ -9,8 +10,25 @@ const AllUsers = () => {
         return res.json();
     })
 
-    const handleMakeAdmin = id = {
-        
+    const handleMakeAdmin = user => {
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: `${user.name} is an Admin now!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
+
     }
 
     const handleDelete = user => {
@@ -39,7 +57,7 @@ const AllUsers = () => {
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
                                 <td>{
-                                    user.role === 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user._id)} className="btn btn-ghost btn-md text-white text-lg bg-red-400"><FaUserShield /></button>
+                                    user.role === 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost btn-md text-white text-lg bg-red-400"><FaUserShield /></button>
                                 }</td>
                                 <td><button onClick={() => handleDelete(user)} className="btn btn-ghost btn-md text-white text-lg bg-red-600"><FaTrashAlt /></button></td>
                             </tr>)
