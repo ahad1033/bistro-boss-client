f030b538a1efb5d67f4192fe149c505af414ce56
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-11-05 19:57:47 +0600

    display all user from admin api

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,16 +1,53 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { FaTrashAlt, FaUserShield } from 'react-icons/fa';
 
 const AllUsers = () => {
 
-    const { data: users = [], refetch} = useQuery(['users'], async() => {
+    const { data: users = [], refetch } = useQuery(['users'], async () => {
         const res = await fetch('http://localhost:5000/users')
         return res.json();
     })
 
+    const handleMakeAdmin = id = {
+        
+    }
+
+    const handleDelete = user => {
+
+    }
+
     return (
-        <div>
-            {users.length}
+        <div className='w-full px-5'>
+            <h3 className="text-3xl font-semibold my-4">Total Users: {users.length}</h3>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map(user => <tr key={user._id}>
+                                <th>{user.index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{
+                                    user.role === 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user._id)} className="btn btn-ghost btn-md text-white text-lg bg-red-400"><FaUserShield /></button>
+                                }</td>
+                                <td><button onClick={() => handleDelete(user)} className="btn btn-ghost btn-md text-white text-lg bg-red-600"><FaTrashAlt /></button></td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
