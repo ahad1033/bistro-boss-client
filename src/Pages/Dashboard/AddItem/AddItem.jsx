158167e02f11977e206e45d945b4cebd013982a7
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-11-13 23:41:10 +0600

    create add item form-79-3-11

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 
 const AddItem = () => {
     return (
-        <div>
-            <h2 className="text-4xl">Add a new Item</h2>
+        <div className='w-full px-10'>
+            <SectionTitle subHeading="What's new" heading="Add an Item" />
+            <form>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text font-semibold">Recipe name</span>
+                    </label>
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Category</span>
+                    </label>
+                    <select className="select select-bordered">
+                        <option disabled selected>Pick one</option>
+                        <option>Pizza</option>
+                        <option>Salad</option>
+                        <option>Soup</option>
+                        <option>Desert</option>
+                        <option>Drinks</option>
+                    </select>
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text font-semibold">Price</span>
+                    </label>
+                    <input type="number" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Recipe details</span>
+                    </label>
+                    <textarea className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Item image</span>
+                    </label>
+                    <input type="file" className="file-input file-input-bordered w-full max-w-xs" />
+                </div>
+                <input className='btn btn-sm mt-4' type="submit" value="Add Item" />
+            </form>
         </div>
     );
 };
